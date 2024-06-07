1ebd04fb3e4e64cc4db328432f2d44f3f709083e
mdsaidul44 <islam88758@gmail.com>
2024-06-07 19:30:57 +0600

    user information relatd api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
- 
+
 
 
 
@@ -31,18 +31,31 @@ async function run() {
 
     // collections 
     const blogCollection = client.db('bloodWavedb').collection('blog')
+    const userCollection = client.db('bloodWavedb').collection('user')
 
     // blog api
-    app.get('/blog',async(req,res)=>{
-      const cursor = blogCollection.find() 
+    app.get('/blog', async (req, res) => {
+      const cursor = blogCollection.find()
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    app.get('/blog/:id' ,async(req,res)=>{
+    app.get('/blog/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result =await blogCollection.findOne(query)
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.findOne(query)
+      res.send(result)
+    })
+
+    // user related api
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
       res.send(result)
     })
 
@@ -58,10 +71,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send('blood wave is running')
+app.get('/', (req, res) => {
+  res.send('blood wave is running')
 })
 
-app.listen(port,()=>{
-    console.log(`blood wave server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`blood wave server is running on port ${port}`)
 })
