54570ff71e226ce20c3cbd70c1e8876f7f7eca4e
mdsaidul44 <islam88758@gmail.com>
2024-06-09 20:09:19 +0600

    volunteer role setup and get volunteer user done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,15 +11,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyToken = (req,res,next)=>{
-  console.log('inside verifyToken',req.headers.authorization);
-  if(!req.headers.authorization){
-    return req.status(401).send({message: 'unauthorized access'})
+const verifyToken = (req, res, next) => {
+  console.log('inside verifyToken', req.headers.authorization);
+  if (!req.headers.authorization) {
+    return req.status(401).send({ message: 'unauthorized access' })
   }
-  const token =req.headers.authorization.split(' '[1])
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET ,(err,decoded)=>{
-    if(err){
-      return req.status(403).send({message: 'forbidden access'})
+  const token = req.headers.authorization.split(' '[1])
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return req.status(403).send({ message: 'forbidden access' })
     }
     req.decoded = decoded;
     next()
@@ -48,23 +49,83 @@ async function run() {
     const requestCollection = client.db('bloodWavedb').collection('request')
 
     // jwt related api
-    app.post('/jwt',async(req,res)=>{
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1hr'})
-      res.send({token})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
     })
 
     // use verify admin after verifyToken
-    const verifyAdmin = async(req,res,next)=>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email: email}
-      const user  = await userCollection.findOne(query)
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
       const isAdmin = user?.role === 'admin'
-      if(!isAdmin){
-        return res.status(403).send({message: 'forbidden'})
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden' })
       }
       next()
     }
+
+
+    // user verify volunteer api
+    const verifyVolunteer = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      const isVolunteer = user?.role === 'volunteer'
+      if (!isVolunteer) {
+        return res.status(403).send({ message: 'forbidden' })
+      }
+      next()
+    }
+
+
+    // user related api
+
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query)
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin'
+      }
+      res.send({ admin })
+    })
+
+    app.get('/user/volunteer/:email', async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query)
+      let volunteer = false;
+      if (user) {
+        volunteer = user?.role === 'volunteer'
+      }
+      res.send({ volunteer })
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
 
 
     // blog api
@@ -81,38 +142,6 @@ async function run() {
       res.send(result)
     })
 
-    // user related api
-    app.post('/users', async (req, res) => {
-      const user = req.body;
-      const query = { email: user.email }
-      const existingUser = await userCollection.findOne(query)
-      if (existingUser) {
-        return res.send({ message: 'user already exists', insertedId: null })
-      }
-      const result = await userCollection.insertOne(user)
-      res.send(result)
-    })
-
-    app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray()
-      res.send(result)
-    })
-
-    app.get('/users/admin/:email',async(req,res)=>{
-      const email = req.params.email;
-      if(email !==req.decoded.email){
-        return res.status(403).send({message: 'forbidden access'}) 
-      }
-      const query = {email: email};
-      const user = await userCollection.findOne(query)
-      let admin = false;
-      if(user){
-        admin = user?.role === 'admin'
-      }
-      res.send({admin})
-    })
-
-    
 
     // request related api
 
@@ -150,19 +179,19 @@ async function run() {
           requesterName: data.requesterName,
           requesterEmail: data.requesterEmail,
           recipientName: data.recipientName,
-          recipientAddress: data.recipientAddress, 
+          recipientAddress: data.recipientAddress,
           donationDate: data.donationDate,
           donationTime: data.donationTime,
           bloodGroup: data.bloodGroup,
-          requestMessage: data.requestMessage, 
+          requestMessage: data.requestMessage,
         }
       }
-      const result = await requestCollection.updateOne(filter,updateReq)
+      const result = await requestCollection.updateOne(filter, updateReq)
       res.send(result)
 
     })
 
-    
+
     app.delete('/requests/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
