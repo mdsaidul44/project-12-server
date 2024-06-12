7bb01b874c6baf42b52e3239c776aa46a09bddc9
mdsaidul44 <islam88758@gmail.com>
2024-06-12 21:00:12 +0600

    all api complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
@@ -330,6 +331,23 @@ async function run() {
       const result = await requestCollection.deleteOne(query)
       res.send(result)
     })
+
+    // payment api 
+    // app.post('/create-payment-intent',async(req,res)=>{
+    //   const {price} = req.body;
+    //   const amount = parseInt(price * 100)
+    //   console.log(amount,'amount is heare')
+
+    //   const paymentIntent = await stripe.paymentIntents.create({
+    //     amount: amount,
+    //     currency: 'usd',
+    //     payment_method_types: ['card']
+    //   })
+ 
+    //   res.send({
+    //     clientSecret: paymentIntent.client_secret
+    //   })
+    // })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
