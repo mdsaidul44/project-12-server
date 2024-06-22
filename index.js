d9fcf9a7df19a7609e7a86df8b1474c5dbaaefd2
mdsaidul44 <islam88758@gmail.com>
2024-06-22 11:05:29 +0600

    all ok

--- index.js
+++ index.js
@@ -268,6 +268,15 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/request',async(req,res) =>{
+      const id = prq.params.id;
+      const query = {
+        $and:{
+          
+        }
+      }
+    })
+
     app.get('/request/:email', async (req, res) => {
       const email = req.params.email;
       const query = { requesterEmail: email }
@@ -350,8 +359,8 @@ async function run() {
     // })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
